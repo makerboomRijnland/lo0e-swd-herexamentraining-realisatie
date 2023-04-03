a2e7ea8d21842463fb45e4156633ea4c6b0edb03
makerboomRijnland <makerboom@mborijnland.nl>
2023-04-03 11:47:01 +0200

    Update admin/movies/edit.php to show values from the database.

--- src/admin/movies/edit.php
+++ src/admin/movies/edit.php
@@ -1,3 +1,13 @@
+<?php 
+    require_once '../../lib/conf.php';
+    require_once '../../models/movie.php';
+
+    $movie = Movie::get($_GET['id']);
+    if(is_null($movie)) {
+        die("No movie!");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,19 +40,19 @@
 
         <dl>
             <dt>ID</dt>
-            <dd><input type="number" name="movie[id]" id="movie_id" value="1"></dd>
+            <dd><input type="number" name="movie[id]" id="movie_id" value="<?= $movie->id ?>"></dd>
 
             <dt>Title</dt>
-            <dd><input type="text" name="movie[title]" id="movie_id" value="Movie A"></dd>
+            <dd><input type="text" name="movie[title]" id="movie_id" value="<?= $movie->title ?>"></dd>
 
             <dt>Description</dt>
-            <dd><textarea name="move[description" id="movie_description" cols="30" rows="10">Movie A is the best movie you can watch.</textarea></dd>
+            <dd><textarea name="move[description" id="movie_description" cols="30" rows="10" maxlength="255"><?= $movie->description ?></textarea></dd>
 
             <dt>Release year</dt>
-            <dd><input type="number" name="movie[release_year]" id="movie_release_year" value="2021" minlength="4" maxlength="4"></dd>
+            <dd><input type="number" name="movie[release_year]" id="movie_release_year" value="<?= $movie->release_year ?>" minlength="4" maxlength="4"></dd>
 
             <dt>Length</dt>
-            <dd><input type="number" name="movie[length]" id="movie_length" value="2"> hrs</dd>
+            <dd><input type="number" name="movie[length]" id="movie_length" value="<?= $movie->length ?>"> hrs</dd>
 
             <dt>Special features</dt>
             <dd>None</dd>
@@ -52,23 +62,18 @@
 
         <dl>
             <dt>Rental duration</dt>
-            <dd><input type="number" name="movie[rental_duration]" id="movie_rental_duration" value="7"> days</dd>   
+            <dd><input type="number" name="movie[rental_duration]" id="movie_rental_duration" value="<?= $movie->rental_duration ?>"> days</dd>   
 
             <dt>Rental rate</dt>
-            <dd>&dollar; <input type="number" name="movie[rental_rate]" id="movie_rental_rate" value="7"></dd>
+            <dd>&dollar; <input type="number" name="movie[rental_rate]" id="movie_rental_rate" value="<?= $movie->rental_rate ?>"></dd>
 
             <dt>Replacement cost</dt>
-            <dd>&dollar; <input type="number" name="movie[replacement_cost]" id="movie_replacement_cost" value="54"></dd>
+            <dd>&dollar; <input type="number" name="movie[replacement_cost]" id="movie_replacement_cost" value="<?= $movie->replacement_cost ?>"></dd>
         </dl>
 
         <h3>Full text</h3>
 
-        <textarea name="move[full_text]" id="movie_full_text" cols="30" rows="10">
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-Dolor eaque voluptatum at quod maxime delectus eos, 
-excepturi itaque labore obcaecati ullam reiciendis hic quasi, ipsum minima. 
-Nostrum eveniet ullam fugit!
-        </textarea>
+        <textarea name="move[full_text]" id="movie_full_text" cols="30" rows="10"><?= $movie->full_text ?></textarea>
     </form>
     
     
